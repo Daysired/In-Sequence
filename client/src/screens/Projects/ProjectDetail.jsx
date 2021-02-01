25fe5e812832d8b81d58eb921473af2de424aaad
Daysired De Jesus <daisy@Daysireds-MacBook-Pro.local>
2021-02-01 12:06:50 -0500

    StudyDetail and ProjectDetail complete

---
+++ client/src/screens/Projects/ProjectDetail.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom'
+
+import Layout from '../../layouts/Layout';
+
+export default function ProjectDetail(props) {
+  const [project, setProject] = useState(null);
+  const { projects, handleDelete } = props;
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (projects.length) {
+      const oneProject = projects.find((project) => project.id === Number(id));
+      setProject(oneProject)
+    }
+  }, [projects, id])
+
+  return (
+    <Layout>
+      <div className="project-details">
+        {
+          study &&
+          <div className="project-card">
+            <h3>{project.name}</h3>
+            <img src={project.img_url} alt={project.name} />
+            <p>{project.description}</p>
+            <p>{project.deployed_url}</p>
+            <p>{project.stack}</p>
+            <Link to={`/projects/${project.id}/edit`}><button>Edit</button></Link>
+            <button onClick={() => handleDelete(project.id)}>Delete</button>
+          </div>
+        }
+      </div>
+    </Layout>
+  )
+}
---
+++ client/src/screens/Studies/StudyDetail.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom'
+
+import Layout from '../../layouts/Layout';
+
+export default function StudyDetail(props) {
+  const [study, setStudy] = useState(null);
+  const { studies, handleDelete } = props;
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (studies.length) {
+      const oneStudy = studies.find((study) => study.id === Number(id));
+      setStudy(oneStudy)
+    }
+  }, [studies, id])
+
+  return (
+    <Layout>
+      <div className="study-details">
+        {
+          study &&
+          <div className="study-card">
+            <h3>{study.topic}</h3>
+            <img src={study.logo_url} alt={study.topic} />
+            <p>{study.description}</p>
+            <p>{study.documentation_url}</p>
+            <Link to={`/studies/${study.id}/edit`}><button>Edit</button></Link>
+            <button onClick={() => handleDelete(study.id)}>Delete</button>
+          </div>
+        }
+      </div>
+    </Layout>
+  )
+}
