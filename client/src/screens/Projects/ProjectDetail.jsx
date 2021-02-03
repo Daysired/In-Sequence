1d8c73851c80527557bd05ffb6e609c4cbcdcb37
Daysired De Jesus <daisy@Daysireds-MacBook-Pro.local>
2021-02-03 06:33:32 -0500

    styled ProjectDetail screen

--- client/src/screens/Projects/ProjectDetail.jsx
+++ client/src/screens/Projects/ProjectDetail.jsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom'
 import './ProjectDetail.css';
 
-import Layout from '../../layouts/Layout';
-
 export default function ProjectDetail(props) {
   const [project, setProject] = useState(null);
   const { projects, handleDelete } = props;
@@ -17,21 +15,19 @@ export default function ProjectDetail(props) {
   }, [projects, id])
 
   return (
-    <Layout>
       <div className="project-details">
         {
           project &&
-          <div className="project-card">
-            <h3>{project.name}</h3>
-            <img src={project.img_url} alt={project.name} />
-            <p>{project.description}</p>
-            <p>{project.deployed_url}</p>
-            <p>{project.stack}</p>
-            <Link to={`/projects/${project.id}/edit`}><button>Edit</button></Link>
-            <button onClick={() => handleDelete(project.id)}>Delete</button>
+          <div className="pr-detail-card">
+            <img className="pr-detail-img" src={project.img_url} alt={project.name} />
+            <h3 className="pr-name">{project.name}</h3>
+            <p className="pr-description">Description: {project.description}</p>
+            <p className="pr-deployed">Deployed Url: {project.deployed_url}</p>
+            <p className="pr-stack">Stack: {project.stack}</p>
+            <Link to={`/projects/${project.id}/edit`}><button className="pr-detail-edit">Edit</button></Link>
+            <button className="pr-detail-delete"onClick={() => handleDelete(project.id)}>Delete</button>
           </div>
         }
       </div>
-    </Layout>
   )
 }
