7ba1bb1c1e5352f1f619f9f0d82ac2c2ac81128c
Daysired De Jesus <daisy@Daysireds-MacBook-Pro.local>
2021-02-02 22:55:32 -0500

    styled home screen and made small changes to study screen

--- client/src/screens/Home&Landing/Home.jsx
+++ client/src/screens/Home&Landing/Home.jsx
@@ -1,11 +1,31 @@
 import './Home.css';
+import { Link } from 'react-router-dom';
 
 import React from 'react'
 
 export default function Home() {
   return (
     <div>
-      
+      <p className="h1">Get Organized.</p>
+      <p className="h2">Stay Organized.</p>
+      <div className="home-containers">
+        <Link className="st-container" to='/studies'>
+          <img src='https://i.imgur.com/QtFuZbl.png' />
+          <p className="st">Study</p>
+        </Link>
+        <Link className="pr-container" to='/projects'>
+          <img src='https://i.imgur.com/aKQyzwC.png' />
+          <p className="pr">Projects</p>
+        </Link>
+        <Link className="nt-container" to='/network'>
+          <img src='https://i.imgur.com/6y1DsPB.png' />
+          <p className="nt">Network</p>
+        </Link>
+        <Link className="js-container" to='/job-search'>
+          <img src='https://i.imgur.com/qpx4aBj.png' />
+          <p className="js">Job Search</p>
+        </Link>
+      </div>
     </div>
   )
 }
